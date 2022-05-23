b30501c430037c9fdaab2e74a564b7a1c53ac329
XIANGYAO TANG <u1311496@umail.utah.edu>
2022-05-23 13:01:43 -0600

    changedCode

--- receive.py
+++ receive.py
@@ -1,7 +1,8 @@
 import pika
 import time
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
 channel.queue_declare(queue='task_queue', durable=True)
@@ -12,11 +13,10 @@ def callback(ch, method, properties, body):
     print(" [x] Received %r" % body.decode())
     time.sleep(body.count(b'.'))
     print(" [x] Done")
-    ch.basic_ack(delivery_tag = method.delivery_tag)
+    ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
 channel.basic_qos(prefetch_count=1)
-channel.basic_consume('task_queue', callback)
-
+channel.basic_consume(queue='task_queue', on_message_callback=callback)
 
 channel.start_consuming()
